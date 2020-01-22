b968dcda3882089944ad687ab70d09b889d83bc0
tomedalya <tom.medalya@gmail.com>
2020-01-22 16:48:42 +0100

    add onReceivedSslError

--- src/android/com/ionicframework/cordova/webview/IonicWebViewEngine.java
+++ src/android/com/ionicframework/cordova/webview/IonicWebViewEngine.java
@@ -1,22 +1,19 @@
-package com.myApp.webview;
+package com.ionicframework.cordova.webview;
 
-import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.annotation.TargetApi;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.net.http.SslError;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import android.webkit.SslErrorHandler;
+import android.util.Log;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
-
-import com.ionicframework.cordova.webview.IonicWebViewEngine;
-import com.ionicframework.cordova.webview.WebViewLocalServer;
-
 import org.apache.cordova.ConfigXmlParser;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPreferences;
@@ -27,52 +24,97 @@ import org.apache.cordova.NativeToJsMessageQueue;
 import org.apache.cordova.PluginManager;
 import org.apache.cordova.engine.SystemWebViewClient;
 import org.apache.cordova.engine.SystemWebViewEngine;
-
-import java.lang.reflect.Field;
+import org.apache.cordova.engine.SystemWebView;
 
 public class IonicWebViewEngine extends SystemWebViewEngine {
-
     public static final String TAG = "IonicWebViewEngine";
 
     private WebViewLocalServer localServer;
     private String CDV_LOCAL_SERVER;
+    private String scheme;
+    private static final String LAST_BINARY_VERSION_CODE = "lastBinaryVersionCode";
+    private static final String LAST_BINARY_VERSION_NAME = "lastBinaryVersionName";
 
     /**
      * Used when created via reflection.
      */
-    public MyWebViewEngine(Context context, CordovaPreferences preferences) {
-        super(context, preferences);
+    public IonicWebViewEngine(Context context, CordovaPreferences preferences) {
+        super(new SystemWebView(context), preferences);
+        Log.d(TAG, "Ionic Web View Engine Starting Right Up 1...");
+    }
+
+    public IonicWebViewEngine(SystemWebView webView) {
+        super(webView, null);
+        Log.d(TAG, "Ionic Web View Engine Starting Right Up 2...");
+    }
+
+    public IonicWebViewEngine(SystemWebView webView, CordovaPreferences preferences) {
+        super(webView, preferences);
+        Log.d(TAG, "Ionic Web View Engine Starting Right Up 3...");
     }
 
     @Override
     public void init(CordovaWebView parentWebView, CordovaInterface cordova, final CordovaWebViewEngine.Client client,
                      CordovaResourceApi resourceApi, PluginManager pluginManager,
                      NativeToJsMessageQueue nativeToJsMessageQueue) {
-
-        super.init(parentWebView, cordova, client, resourceApi, pluginManager, nativeToJsMessageQueue);
-
         ConfigXmlParser parser = new ConfigXmlParser();
         parser.parse(cordova.getActivity());
 
         String hostname = preferences.getString("Hostname", "localhost");
-        String scheme = preferences.getString("Scheme", "http");
+        scheme = preferences.getString("Scheme", "http");
         CDV_LOCAL_SERVER = scheme + "://" + hostname;
 
-        try {
-            Field f = this.getClass().getSuperclass().getDeclaredField("localServer");
-            f.setAccessible(true);
-            localServer = (WebViewLocalServer) f.get(this);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to create my webview engine. ", e);
+        localServer = new WebViewLocalServer(cordova.getActivity(), hostname, true, parser, scheme);
+        localServer.hostAssets("www");
+
+        webView.setWebViewClient(new ServerClient(this, parser));
+
+        super.init(parentWebView, cordova, client, resourceApi, pluginManager, nativeToJsMessageQueue);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            final WebSettings settings = webView.getSettings();
+            int mode = preferences.getInteger("MixedContentMode", 0);
+            settings.setMixedContentMode(mode);
         }
-        webView.setWebViewClient(new MyWebViewClient(this, parser));
+        SharedPreferences prefs = cordova.getActivity().getApplicationContext().getSharedPreferences(IonicWebView.WEBVIEW_PREFS_NAME, Activity.MODE_PRIVATE);
+        String path = prefs.getString(IonicWebView.CDV_SERVER_PATH, null);
+        if (!isDeployDisabled() && !isNewBinary() && path != null && !path.isEmpty()) {
+            setServerBasePath(path);
+        }
     }
 
-    public class MyWebViewClient extends SystemWebViewClient {
+    private boolean isNewBinary() {
+        String versionCode = "";
+        String versionName = "";
+        SharedPreferences prefs = cordova.getActivity().getApplicationContext().getSharedPreferences(IonicWebView.WEBVIEW_PREFS_NAME, Activity.MODE_PRIVATE);
+        String lastVersionCode = prefs.getString(LAST_BINARY_VERSION_CODE, null);
+        String lastVersionName = prefs.getString(LAST_BINARY_VERSION_NAME, null);
 
+        try {
+            PackageInfo pInfo = this.cordova.getActivity().getPackageManager().getPackageInfo(this.cordova.getActivity().getPackageName(), 0);
+            versionCode = Integer.toString(pInfo.versionCode);
+            versionName = pInfo.versionName;
+        } catch(Exception ex) {
+            Log.e(TAG, "Unable to get package info", ex);
+        }
+
+        if (!versionCode.equals(lastVersionCode) || !versionName.equals(lastVersionName)) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString(LAST_BINARY_VERSION_CODE, versionCode);
+            editor.putString(LAST_BINARY_VERSION_NAME, versionName);
+            editor.putString(IonicWebView.CDV_SERVER_PATH, "");
+            editor.apply();
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isDeployDisabled() {
+        return preferences.getBoolean("DisableDeploy", false);
+    }
+    private class ServerClient extends SystemWebViewClient {
         private ConfigXmlParser parser;
 
-        public MyWebViewClient(SystemWebViewEngine parentEngine, ConfigXmlParser parser) {
+        public ServerClient(SystemWebViewEngine parentEngine, ConfigXmlParser parser) {
             super(parentEngine);
             this.parser = parser;
         }
@@ -97,7 +139,7 @@ public class IonicWebViewEngine extends SystemWebViewEngine {
                 view.stopLoading();
                 // When using a custom scheme the app won't load if server start url doesn't end in /
                 String startUrl = CDV_LOCAL_SERVER;
-                if (!CDV_LOCAL_SERVER.startsWith(WebViewLocalServer.httpsScheme) && !CDV_LOCAL_SERVER.startsWith(WebViewLocalServer.httpScheme)) {
+                if (!scheme.equalsIgnoreCase(WebViewLocalServer.httpsScheme) && !scheme.equalsIgnoreCase(WebViewLocalServer.httpScheme)) {
                     startUrl += "/";
                 }
                 view.loadUrl(startUrl);
@@ -111,11 +153,12 @@ public class IonicWebViewEngine extends SystemWebViewEngine {
                     "window.WEBVIEW_SERVER_URL = '" + CDV_LOCAL_SERVER + "';" +
                     "})()");
         }
-
+        
         @Override
         public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
             // after calling super method we could be able to implement our own logic.
-            super.onReceivedSslError(view, handler, error);
+            super.onReceivedSslError(view, 
+                                     , error);
             Context ctx = view.getContext();
             final String packageName = ctx.getPackageName();
             final PackageManager pm = ctx.getPackageManager();
@@ -131,5 +174,14 @@ public class IonicWebViewEngine extends SystemWebViewEngine {
                 getCordovaWebView().postMessage("onReceivedSslError", error.getPrimaryError());
             }
         }
+    }
+
+    public void setServerBasePath(String path) {
+        localServer.hostFiles(path);
+        webView.loadUrl(CDV_LOCAL_SERVER);
+    }
+
+    public String getServerBasePath() {
+        return this.localServer.getBasePath();
     }
 }
