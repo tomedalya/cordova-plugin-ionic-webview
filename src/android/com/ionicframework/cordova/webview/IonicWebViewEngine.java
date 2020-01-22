84c59c4d00a1bfd0e051b0865d8ad6c1df386662
tomedalya <tom.medalya@gmail.com>
2020-01-22 14:40:23 +0100

    change engine to fix onReceivedSslError
    based on https://medium.com/@shepherdlee/handle-ssl-error-in-ionic-android-app-1973d363147f

--- src/android/com/ionicframework/cordova/webview/IonicWebViewEngine.java
+++ src/android/com/ionicframework/cordova/webview/IonicWebViewEngine.java
@@ -1,19 +1,22 @@
-package com.ionicframework.cordova.webview;
+package com.myApp.webview;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.net.http.SslError;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import android.util.Log;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
+
+import com.ionicframework.cordova.webview.IonicWebViewEngine;
+import com.ionicframework.cordova.webview.WebViewLocalServer;
+
 import org.apache.cordova.ConfigXmlParser;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPreferences;
@@ -24,143 +27,109 @@ import org.apache.cordova.NativeToJsMessageQueue;
 import org.apache.cordova.PluginManager;
 import org.apache.cordova.engine.SystemWebViewClient;
 import org.apache.cordova.engine.SystemWebViewEngine;
-import org.apache.cordova.engine.SystemWebView;
 
-public class IonicWebViewEngine extends SystemWebViewEngine {
-  public static final String TAG = "IonicWebViewEngine";
+import java.lang.reflect.Field;
 
-  private WebViewLocalServer localServer;
-  private String CDV_LOCAL_SERVER;
-  private String scheme;
-  private static final String LAST_BINARY_VERSION_CODE = "lastBinaryVersionCode";
-  private static final String LAST_BINARY_VERSION_NAME = "lastBinaryVersionName";
+public class MyWebViewEngine extends IonicWebViewEngine {
 
-  /**
-   * Used when created via reflection.
-   */
-  public IonicWebViewEngine(Context context, CordovaPreferences preferences) {
-    super(new SystemWebView(context), preferences);
-    Log.d(TAG, "Ionic Web View Engine Starting Right Up 1...");
-  }
+    public static final String TAG = "IonicWebViewEngine";
 
-  public IonicWebViewEngine(SystemWebView webView) {
-    super(webView, null);
-    Log.d(TAG, "Ionic Web View Engine Starting Right Up 2...");
-  }
+    private WebViewLocalServer localServer;
+    private String CDV_LOCAL_SERVER;
 
-  public IonicWebViewEngine(SystemWebView webView, CordovaPreferences preferences) {
-    super(webView, preferences);
-    Log.d(TAG, "Ionic Web View Engine Starting Right Up 3...");
-  }
-
-  @Override
-  public void init(CordovaWebView parentWebView, CordovaInterface cordova, final CordovaWebViewEngine.Client client,
-                   CordovaResourceApi resourceApi, PluginManager pluginManager,
-                   NativeToJsMessageQueue nativeToJsMessageQueue) {
-    ConfigXmlParser parser = new ConfigXmlParser();
-    parser.parse(cordova.getActivity());
-
-    String hostname = preferences.getString("Hostname", "localhost");
-    scheme = preferences.getString("Scheme", "http");
-    CDV_LOCAL_SERVER = scheme + "://" + hostname;
-
-    localServer = new WebViewLocalServer(cordova.getActivity(), hostname, true, parser, scheme);
-    localServer.hostAssets("www");
-
-    webView.setWebViewClient(new ServerClient(this, parser));
-
-    super.init(parentWebView, cordova, client, resourceApi, pluginManager, nativeToJsMessageQueue);
-    if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      final WebSettings settings = webView.getSettings();
-      int mode = preferences.getInteger("MixedContentMode", 0);
-      settings.setMixedContentMode(mode);
-    }
-    SharedPreferences prefs = cordova.getActivity().getApplicationContext().getSharedPreferences(IonicWebView.WEBVIEW_PREFS_NAME, Activity.MODE_PRIVATE);
-    String path = prefs.getString(IonicWebView.CDV_SERVER_PATH, null);
-    if (!isDeployDisabled() && !isNewBinary() && path != null && !path.isEmpty()) {
-      setServerBasePath(path);
-    }
-  }
-
-  private boolean isNewBinary() {
-    String versionCode = "";
-    String versionName = "";
-    SharedPreferences prefs = cordova.getActivity().getApplicationContext().getSharedPreferences(IonicWebView.WEBVIEW_PREFS_NAME, Activity.MODE_PRIVATE);
-    String lastVersionCode = prefs.getString(LAST_BINARY_VERSION_CODE, null);
-    String lastVersionName = prefs.getString(LAST_BINARY_VERSION_NAME, null);
-
-    try {
-      PackageInfo pInfo = this.cordova.getActivity().getPackageManager().getPackageInfo(this.cordova.getActivity().getPackageName(), 0);
-      versionCode = Integer.toString(pInfo.versionCode);
-      versionName = pInfo.versionName;
-    } catch(Exception ex) {
-      Log.e(TAG, "Unable to get package info", ex);
-    }
-
-    if (!versionCode.equals(lastVersionCode) || !versionName.equals(lastVersionName)) {
-      SharedPreferences.Editor editor = prefs.edit();
-      editor.putString(LAST_BINARY_VERSION_CODE, versionCode);
-      editor.putString(LAST_BINARY_VERSION_NAME, versionName);
-      editor.putString(IonicWebView.CDV_SERVER_PATH, "");
-      editor.apply();
-      return true;
-    }
-    return false;
-  }
-
-  private boolean isDeployDisabled() {
-    return preferences.getBoolean("DisableDeploy", false);
-  }
-  private class ServerClient extends SystemWebViewClient {
-    private ConfigXmlParser parser;
-
-    public ServerClient(SystemWebViewEngine parentEngine, ConfigXmlParser parser) {
-      super(parentEngine);
-      this.parser = parser;
-    }
-
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
-      return localServer.shouldInterceptRequest(request.getUrl(), request);
-    }
-
-    @TargetApi(Build.VERSION_CODES.KITKAT)
-    @Override
-    public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
-      return localServer.shouldInterceptRequest(Uri.parse(url), null);
+    /**
+     * Used when created via reflection.
+     */
+    public MyWebViewEngine(Context context, CordovaPreferences preferences) {
+        super(context, preferences);
     }
 
     @Override
-    public void onPageStarted(WebView view, String url, Bitmap favicon) {
-      super.onPageStarted(view, url, favicon);
-      String launchUrl = parser.getLaunchUrl();
-      if (!launchUrl.contains(WebViewLocalServer.httpsScheme) && !launchUrl.contains(WebViewLocalServer.httpScheme) && url.equals(launchUrl)) {
-        view.stopLoading();
-        // When using a custom scheme the app won't load if server start url doesn't end in /
-        String startUrl = CDV_LOCAL_SERVER;
-        if (!scheme.equalsIgnoreCase(WebViewLocalServer.httpsScheme) && !scheme.equalsIgnoreCase(WebViewLocalServer.httpScheme)) {
-          startUrl += "/";
+    public void init(CordovaWebView parentWebView, CordovaInterface cordova, final CordovaWebViewEngine.Client client,
+                     CordovaResourceApi resourceApi, PluginManager pluginManager,
+                     NativeToJsMessageQueue nativeToJsMessageQueue) {
+
+        super.init(parentWebView, cordova, client, resourceApi, pluginManager, nativeToJsMessageQueue);
+
+        ConfigXmlParser parser = new ConfigXmlParser();
+        parser.parse(cordova.getActivity());
+
+        String hostname = preferences.getString("Hostname", "localhost");
+        String scheme = preferences.getString("Scheme", "http");
+        CDV_LOCAL_SERVER = scheme + "://" + hostname;
+
+        try {
+            Field f = this.getClass().getSuperclass().getDeclaredField("localServer");
+            f.setAccessible(true);
+            localServer = (WebViewLocalServer) f.get(this);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create my webview engine. ", e);
         }
-        view.loadUrl(startUrl);
-      }
+        webView.setWebViewClient(new MyWebViewClient(this, parser));
     }
 
-    @Override
-    public void onPageFinished(WebView view, String url) {
-      super.onPageFinished(view, url);
-      view.loadUrl("javascript:(function() { " +
-              "window.WEBVIEW_SERVER_URL = '" + CDV_LOCAL_SERVER + "';" +
-              "})()");
+    public class MyWebViewClient extends SystemWebViewClient {
+
+        private ConfigXmlParser parser;
+
+        public MyWebViewClient(SystemWebViewEngine parentEngine, ConfigXmlParser parser) {
+            super(parentEngine);
+            this.parser = parser;
+        }
+
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+            return localServer.shouldInterceptRequest(request.getUrl(), request);
+        }
+
+        @TargetApi(Build.VERSION_CODES.KITKAT)
+        @Override
+        public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+            return localServer.shouldInterceptRequest(Uri.parse(url), null);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            String launchUrl = parser.getLaunchUrl();
+            if (!launchUrl.contains(WebViewLocalServer.httpsScheme) && !launchUrl.contains(WebViewLocalServer.httpScheme) && url.equals(launchUrl)) {
+                view.stopLoading();
+                // When using a custom scheme the app won't load if server start url doesn't end in /
+                String startUrl = CDV_LOCAL_SERVER;
+                if (!CDV_LOCAL_SERVER.startsWith(WebViewLocalServer.httpsScheme) && !CDV_LOCAL_SERVER.startsWith(WebViewLocalServer.httpScheme)) {
+                    startUrl += "/";
+                }
+                view.loadUrl(startUrl);
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            view.loadUrl("javascript:(function() { " +
+                    "window.WEBVIEW_SERVER_URL = '" + CDV_LOCAL_SERVER + "';" +
+                    "})()");
+        }
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            // after calling super method we could be able to implement our own logic.
+            super.onReceivedSslError(view, handler, error);
+            Context ctx = view.getContext();
+            final String packageName = ctx.getPackageName();
+            final PackageManager pm = ctx.getPackageManager();
+            ApplicationInfo appInfo;
+            try {
+                appInfo = pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
+                if ((appInfo.flags & ApplicationInfo.FLAG_DEBUGGABLE) == 0) {
+                    // debug = false
+                    getCordovaWebView().postMessage("onReceivedSslError", error.getPrimaryError());
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                // When it doubt, lock it out!
+                getCordovaWebView().postMessage("onReceivedSslError", error.getPrimaryError());
+            }
+        }
     }
-  }
-
-  public void setServerBasePath(String path) {
-    localServer.hostFiles(path);
-    webView.loadUrl(CDV_LOCAL_SERVER);
-  }
-
-  public String getServerBasePath() {
-    return this.localServer.getBasePath();
-  }
 }
